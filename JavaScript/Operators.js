beaf4efde75ba79a8f02d9151d87bdd02cd54161
mohitrajora <mohitrajora1432@gmail.com>
2025-06-20 12:10:04 +0530

    .

--- JavaScript/Operators.js
+++ JavaScript/Operators.js
@@ -119,3 +119,152 @@
 // let num17 = 0;
 // let result = num16++ + num17++ && num16 || num17;
 // console.log(result);
+
+
+// -------------------------- tarnary op -------------------------------
+
+// (condition)?"True part":"False part"
+
+// let num1 = 0;
+// let result = (num1)?"yes":"no";
+// console.log(result);
+
+// let num2 = 10;
+// let num3 = 20;
+// let result = (num2 > num3)?num2:num3;
+// console.log(result);
+
+// let a = 10;
+// let b = 20;
+// let c = 30;
+// // let result = ((a>b && a>c) || (b>c && b>a) || (c>a && c>b))?
+// let result = (a>b && a>c)?"a":(b>c)?"b":"c";
+// console.log(result);
+
+// let num3 = 10;
+// if(num3){
+//     console.log("yes");
+// }
+
+// let num4 = 20;
+// if(num4){
+//     console.log("yes");
+// }
+// else{
+//     console.log("no");
+// }
+
+// let num5 = 200;
+// if(num5) console.log("yes");
+// else console.log("no");
+
+// let num = 0;
+// // if(/*num>0 || num<0*/ num!==0){
+// //     console.log("False");
+// // }
+// if(num){
+//     console.log("false");
+// }
+
+// let a = "";
+// if(a){
+//     console.log(!a);
+// }
+// else{
+//     console.log("true");
+// }
+
+// let age = 18;
+// if(age >= 18){
+//     console.log("Vote For MODI!!!");
+// }
+// else{
+//     console.log("Vote For Sbko");
+// }
+
+// let count = 0;
+// let number = -6;
+// if (number) {
+//     count++;
+// }
+// else {
+//     count--;
+// }
+// console.log(count);
+
+// let userName = "hello";
+// let login = true;
+// let status = "active";
+
+// if (userName == "hello") {
+//     if (status == "active") {
+//         console.log(login);
+//     }
+// }
+// else {
+//     login = false;
+//     console.log(login);
+// }
+
+// ---------------------- Switch Case ------------------------
+
+// let choice = "a";
+// switch (choice) {
+//     case "a":
+//         console.log("First");
+//         break;
+//     case "b":
+//         console.log("Second");
+//         break;
+    
+//     default:
+//         console.log("Default ");
+//         break;
+// }
+
+// let choice = true;
+// switch (choice) {
+//     case "true":
+//         console.log("First");
+//         break;
+//     case "true":
+//         console.log("Second");
+//         break;
+    
+//     default:
+//         console.log("Default ");
+//         break;
+// }
+
+// let choice = "positive";
+// switch (choice) {
+//     case -10:
+//         console.log("First");
+//         break;
+//     case "b":
+//         console.log("Second");
+//         break;
+    
+//     default:
+//         console.log("Default ");
+//         break;
+// }
+
+let choice = [];
+switch (choice) {
+    case []:
+        console.log("First");
+        break;
+    case "[]":
+        console.log("Second");
+        break;
+    case true:
+        console.log("third");
+        break;
+    case false:
+        console.log("four");
+        break;
+    default:
+        console.log("Default");
+        break;
+}
